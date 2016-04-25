e24ccb8b4db4e2c3ecc2615cde07def4c4c4b2a1
Dan <dan@cusco.local>
2016-04-25 16:16:32 +0100

    change security group to reference itself

--- terraform/bosh-aws-base.tf
+++ terraform/bosh-aws-base.tf
@@ -80,16 +80,16 @@ resource "aws_security_group" "boshdefault" {
 
 	ingress {
 		from_port   = 0
-		to_port     = 0
+		to_port     = 65535
 		protocol    = "tcp"
-		cidr_blocks = ["10.0.0.0/24"]
+		self        = true
 	}
 
 	ingress {
 		from_port   = 0
-		to_port     = 0
+		to_port     = 65535
 		protocol    = "udp"
-		cidr_blocks = ["10.0.0.0/24"]
+		self        = true
 	}
 
 	# outbound internet access
