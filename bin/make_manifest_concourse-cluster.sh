2da116979b75fb822c1c8f8549d3dcb15c487079
Dan Young <dan.young@engineerbetter.com>
2016-06-28 15:13:10 +0100

    update cluster manifest generation to github auth, garden-runc

--- bin/make_manifest_concourse-cluster.sh
+++ bin/make_manifest_concourse-cluster.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 #
 #  Please set the following environment variables:
-#  $CONCOURSE_PASSWORD
+#  $DB_PASSWORD
 #  $CONCOURSE_URL
+#  $GITHUB_ORG
+#  $GITHUB_CLIENT_ID
+#  $GITHUB_CLIENT_SECRET
 
 DIRECTOR_UUID=`bosh status --uuid`
 
 echo "director_uuid = $DIRECTOR_UUID"
 echo "concourse url = $CONCOURSE_URL"
+echo "GitHub Org = $GITHUB_ORG"
 
 cat >concourse.yml <<YAML
 ---
@@ -18,7 +22,7 @@ director_uuid: $DIRECTOR_UUID
 releases:
 - name: concourse
   version: latest
-- name: garden-linux
+- name: garden-runc
   version: latest
 
 stemcells:
@@ -32,7 +36,7 @@ instance_groups:
   vm_type: concourse_web
   stemcell: trusty
   azs: [z1]
-  networks: [{name: default}]
+  networks: [{name: ops_services}]
   jobs:
   - name: atc
     release: concourse
@@ -40,9 +44,13 @@ instance_groups:
       # replace with your CI's externally reachable URL e.g https://blah
       external_url: $CONCOURSE_URL
 
-      # replace with username/password, or configure GitHub auth
-      basic_auth_username: admin
-      basic_auth_password: $CONCOURSE_PASSWORD
+      # configure GitHub auth
+      github_auth:
+        authorize:
+        - organization: $GITHUB_ORG
+          teams: [CI]
+        client_id: $GITHUB_CLIENT_ID
+        client_secret: $GITHUB_CLIENT_SECRET
 
       postgresql_database: &atc_db atc
   - name: tsa
@@ -55,7 +63,7 @@ instance_groups:
   stemcell: trusty
   persistent_disk_type: default
   azs: [z1]
-  networks: [{name: default}]
+  networks: [{name: ops_services}]
   jobs:
   - name: postgresql
     release: concourse
@@ -64,14 +72,14 @@ instance_groups:
       - name: *atc_db
         # make up a role and password
         role: dbrole
-        password: $CONCOURSE_PASSWORD
+        password: $DB_PASSWORD
 
 - name: worker
   instances: 1
   vm_type: concourse_worker
   stemcell: trusty
   azs: [z1]
-  networks: [{name: default}]
+  networks: [{name: ops_services}]
   jobs:
   - name: groundcrew
     release: concourse
@@ -80,7 +88,7 @@ instance_groups:
     release: concourse
     properties: {}
   - name: garden
-    release: garden-linux
+    release: garden-runc
     properties:
       garden:
         listen_network: tcp
